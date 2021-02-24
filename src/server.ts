ef74c4ff0b6874a7689781a864aaf744872604f1
Brendow Alves <alves.brendow.work@gmail.com>
2021-02-24 15:56:14 -0300

    configured database typorm; migration, created: model, controller, created router

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,11 @@
+import 'reflect-metadata';
+import './database';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Aletração especifica
- */
+app.use(express.json());
+app.use(router);
 
-//http://localhost:3333/users
- app.get("/", (resquest, response) => {
-    //  return response.send("Hello World - NLW04")
-    return response.json({message: "Hello World - NLW04"})
- })
-
-
- // 1 parms => Rota(Recurso API)
- // 2 params => request, response
- app.post("/", (resquest, response) =>{ 
-    //Recebeu =>  resques,response
-    return response.json({message: "Os dados foram salvos com sucesso!"})
-
- })
 app.listen(3333,() =>  console.log("Server is running"))
